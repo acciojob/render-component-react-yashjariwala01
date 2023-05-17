f77c7d004ae9fdbb3cad0dc8e0b030c94f7b5ab1
yashjariwala01 <jariwala.yash02@gmail.com>
2023-05-17 19:19:09 +0530

    submitting code-Wed May 17 2023 19:19:05 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div  id="main">
-      <p> life's good.</p>
+    <div id="main">
+      <p>
+      I am learning React. My life is getting better.
+      </p>
+     
     </div>
   )
 }
